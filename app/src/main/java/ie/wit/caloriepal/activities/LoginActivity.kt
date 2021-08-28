f768ed6dfc571e0eb2fa7d1f754856b8403c2cea
Austin Heraughty <austinmartinh@gmail.com>
2021-08-28 04:34:51 +0100

    firebase auth login added

---
+++ app/src/main/java/ie/wit/caloriepal/activities/LoginActivity.kt
@@ -0,0 +1,215 @@
+package ie.wit.caloriepal.activities
+
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import ie.wit.caloriepal.R
+import ie.wit.caloriepal.helpers.createLoader
+import ie.wit.caloriepal.helpers.hideLoader
+import ie.wit.caloriepal.helpers.showLoader
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.startActivity
+
+
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    private lateinit var auth: FirebaseAuth
+    lateinit var loader: AlertDialog
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Buttons
+        emailSignInButton.setOnClickListener(this)
+        emailCreateAccountButton.setOnClickListener(this)
+        signOutButton.setOnClickListener(this)
+        verifyEmailButton.setOnClickListener(this)
+
+
+        auth = FirebaseAuth.getInstance()
+        loader = createLoader(this)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun createAccount(email: String, password: String) {
+        Log.d(TAG, "createAccount:$email")
+        if (!validateForm()) {
+            return
+        }
+
+        showLoader(loader, "Creating Account...")
+
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    updateUI(null)
+                }
+                hideLoader(loader)
+            }
+        // [END create_user_with_email]
+    }
+
+    private fun signIn(email: String, password: String) {
+        Log.d(TAG, "signIn:$email")
+        if (!validateForm()) {
+            return
+        }
+
+        showLoader(loader, "Logging In...")
+
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    updateUI(null)
+                }
+
+
+                if (!task.isSuccessful) {
+                    loginStatus.setText(R.string.auth_failed)
+                }
+                hideLoader(loader)
+            }
+        // [END sign_in_with_email]
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        updateUI(null)
+    }
+
+    private fun sendEmailVerification() {
+        // Disable button
+        verifyEmailButton.isEnabled = false
+
+        // Send verification email
+        // [START send_email_verification]
+        val user = auth.currentUser
+        user?.sendEmailVerification()
+            ?.addOnCompleteListener(this) { task ->
+                // [START_EXCLUDE]
+                // Re-enable button
+                verifyEmailButton.isEnabled = true
+
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        baseContext,
+                        "Verification email sent to ${user.email} ",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Log.e(TAG, "sendEmailVerification", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Failed to send verification email.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                // [END_EXCLUDE]
+            }
+        // [END send_email_verification]
+    }
+
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val email = fieldEmail.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            fieldEmail.error = "Required."
+            valid = false
+        } else {
+            fieldEmail.error = null
+        }
+
+        val password = fieldPassword.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            fieldPassword.error = "Required."
+            valid = false
+        } else {
+            fieldPassword.error = null
+        }
+
+        return valid
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        hideLoader(loader)
+        if (user != null) {
+            loginStatus.text = getString(
+                R.string.emailpassword_status_fmt,
+                user.email, user.isEmailVerified
+            )
+            detail.text = getString(R.string.firebase_status_fmt, user.uid)
+
+            emailPasswordButtons.visibility = View.GONE
+            emailPasswordFields.visibility = View.GONE
+            signedInButtons.visibility = View.VISIBLE
+
+            verifyEmailButton.isEnabled = !user.isEmailVerified
+            startActivity<HomeActivity>()
+        } else {
+            loginStatus.setText(R.string.signed_out)
+            detail.text = null
+
+            emailPasswordButtons.visibility = View.VISIBLE
+            emailPasswordFields.visibility = View.VISIBLE
+            signedInButtons.visibility = View.GONE
+        }
+    }
+
+    override fun onClick(v: View) {
+        val i = v.id
+        when (i) {
+            R.id.emailCreateAccountButton -> createAccount(
+                fieldEmail.text.toString(),
+                fieldPassword.text.toString()
+            )
+            R.id.emailSignInButton -> signIn(
+                fieldEmail.text.toString(),
+                fieldPassword.text.toString()
+            )
+            R.id.signOutButton -> signOut()
+            R.id.verifyEmailButton -> sendEmailVerification()
+        }
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
+    }
+}
---
+++ app/src/main/java/ie/wit/caloriepal/helpers/Loaders.kt
@@ -0,0 +1,28 @@
+package ie.wit.caloriepal.helpers
+
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.FragmentActivity
+import ie.wit.caloriepal.R
+
+fun createLoader(activity: FragmentActivity) : AlertDialog {
+    val loaderBuilder = AlertDialog.Builder(activity)
+        .setCancelable(true) // 'false' if you want user to wait
+        .setView(R.layout.loading)
+    var loader = loaderBuilder.create()
+    loader.setTitle(R.string.app_name)
+    loader.setIcon(R.drawable.ic_baseline_fastfood_36)
+
+    return loader
+}
+
+fun showLoader(loader: AlertDialog, message: String) {
+    if (!loader.isShowing()) {
+        loader.setTitle(message)
+        loader.show()
+    }
+}
+
+fun hideLoader(loader: AlertDialog) {
+    if (loader.isShowing)
+        loader.dismiss()
+}
